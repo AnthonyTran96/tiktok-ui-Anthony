1710efe1035b64fe479e7474a988729f08072798
Anthony Tran <quanghust96@gmail.com>
2023-03-10 19:26:53 +0700

    them phong

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,26 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder={cx('Search accounts and videos')} />
+                    <button className={cx('close')}></button>
+                    <button className={cx('loading')}></button>
+                    <button className={cx('search-btn')}></button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
