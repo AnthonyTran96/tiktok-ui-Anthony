0efdc0a8f2d4aa9edc90b45c20088a9eb28ee396
Anthony Tran <quanghust96@gmail.com>
2023-03-14 09:58:44 +0700

    them phan search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,8 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import images from '~/assets';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -13,9 +15,15 @@ function Header() {
                 </div>
                 <div className={cx('search')}>
                     <input placeholder={cx('Search accounts and videos')} />
-                    <button className={cx('close')}></button>
-                    <button className={cx('loading')}></button>
-                    <button className={cx('search-btn')}></button>
+                    <button className={cx('close')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <button className={cx('loading')}>
+                        <FontAwesomeIcon icon={faSpinner} />
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </div>
                 <div className={cx('actions')}></div>
             </div>
